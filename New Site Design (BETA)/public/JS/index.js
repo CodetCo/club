b3c5e8fc177129ae1a83fe920135bc981a015022
Aidan Smith <lolbot55555@gmail.com>
2019-08-27 20:43:09 -0700

    Navbar Support

--- New Site Design (BETA)/public/JS/index.js
+++ New Site Design (BETA)/public/JS/index.js
@@ -1,6 +1,72 @@
-var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
-}
+console.log("INIT");
+
+
+$(function() {
+    $(window).scroll(function() {
+       // console.log('scrolling ', $(window).scrollTop(), $(document).height());
+        if($(window).scrollTop() >= 200 && $(window).scrollTop() <= ($(document).height() - 500)) {
+            $('#mainNav').removeClass('hide');
+        } else {
+            $('#mainNav').addClass('hide');
+        }
+    });
+});
+
+
+//document.getElementById('us').scrollIntoView({block: 'start', behavior: 'smooth'});
+
+(function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 20)
+          }, 500, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+  
+  })(jQuery); // End of use strict
+  
