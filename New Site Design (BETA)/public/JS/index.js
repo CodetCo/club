e6887e79c2887f5f6c9b25d2fa4bd1f933ab3112
Aidan Smith <lolbot55555@gmail.com>
2019-08-30 16:25:38 -0700

    Gave credit; fixed mobile bug at cost.

--- New Site Design (BETA)/public/JS/index.js
+++ New Site Design (BETA)/public/JS/index.js
@@ -1,17 +1,12 @@
 console.log("INIT");
 
 //Change mobile view to desktop..... it's not a lazy fix at all
-var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
-}
+
 
 //Scrolling Jquery Function
 (function($) {
     "use strict"; // Start of use strict
-  
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
